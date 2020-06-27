43dd550f37616da8603fcaca4d587904c04a1ed7
Chad Crowell <chad.crowell@linuxacademy.com>
2020-06-27 13:03:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const server = http.createServer((req, res) => {
     }
 });
 // comment
-// comment
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
