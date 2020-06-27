19a46103a855e5e3d9f0bfd3c617413f111d520c
Chad Crowell <chad.crowell@linuxacademy.com>
2020-06-27 14:17:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Hello, World</h1>');
-    }
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+        }
+    );
+}
+
 });
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
